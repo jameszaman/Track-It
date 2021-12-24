6801764ca0f15f85b827371cda07f1009c647fc5
James Zaman <jzaman182040@bscse.uiu.ac.bd>
2021-12-24 18:09:27 +0600

    Setup folder structure along with a basic route.

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require('express');
 
+// Importing routes.
+const mainRoutes = require('./routes/mainRoutes');
+
 const app = express();
 
+// Setting up app.
+// app configs
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Setting static folders.
+app.use(express.static('public'));
+app.use('/js', express.static(__dirname + '/public/js'));
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/images', express.static(__dirname + '/public/images'));
+app.use('/favicon.ico', express.static(__dirname + '/favicon.ico'));
+// Showing favicon.
+app.use("/favicon.ico", express.static(__dirname + "/favicon.ico"));
+app.set('view engine', 'ejs');
+
+// Setting up routes.
+app.use('/', mainRoutes);
+
+// Starting the server.
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
